54bac3369b9070b9c7da65bf9972c65cc2701451
Julia Pontes <47482821+juliapcp@users.noreply.github.com>
2019-03-12 21:37:12 -0300

    Add files via upload

---
+++ caesar.js
@@ -0,0 +1,60 @@
+// caesar.js // 1.0 pts
+
+// 0.1 pts // easy
+// encrypt a string
+// unrestricted
+// input: a to z lowercase only,
+// without accents, special chars (symbols) and spaces
+function encrypt(msg, shift) {
+    const alfabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let newmsg = '';
+    //msg = msg.toLowerCase();
+    for (i = 0; i < msg.length; i++) {
+        //if (alfabet.indexOf(msg[i]) !== -1){
+        newmsg += String.fromCharCode((msg.charCodeAt(i) - 97 + shift) % 26 + 97);
+        //}
+    }
+    return newmsg;
+}
+let msg = 'tads';
+let shift = 1;
+let resp = encrypt(msg, shift);
+console.log(resp); // ubet
+console.log(resp === 'ubet'); // true
+console.assert(resp === 'ubet');
+
+resp = encrypt(msg, 9);
+console.log(resp); // cjmb
+console.log(resp === 'cjmb'); // true
+console.assert(resp === 'cjmb');
+
+// 0.2 pts // medium
+// encrypt a string
+// unrestricted
+// input: any character
+// desconsider any non-a-to-z (accented letters, spaces, special chars (symbols), etc) to encrypt
+// uppercase letters inputs must output lowercase letters
+console.assert(encrypt('BANANA', 3) === 'kjwjwj');
+console.assert(encrypt('teste criptografia', 3) === 'cnbcn larycxpajorj');
+console.assert(encrypt('@#%%&*^nEo*<>;; @', 15) === '@#%%&*^ctd*<>;; @');
+
+// 0.2 pts // medium
+// decrypt a string
+// unrestricted
+// input: any character
+// desconsider any non-a-to-z (spaces, special chars (symbols), etc) to decrypt
+// output always in lowercase
+console.assert(decrypt('kjwjwj', 3) === 'banana');
+console.assert(decrypt('cnbcn larycxpajorj', 3) === 'teste criptografia');
+console.assert(decrypt('@#%%&*^ctd*<>;; @', 15) === '@#%%&*^neo*<>;; @');
+
+// 0.5 pts // hard
+// encrypt and decrypt a string
+// input: any character
+// desconsider any non-a-to-z (spaces, special chars (symbols), etc)
+// output always in lowercase
+// String, Number, RegExp, Math and Array methods, every built-in function not allowed
+// except Array.length, Array[index], String.length, String.charAt and String[index]
+console.assert(encrypt('@# hard %çá', 30) === '@# levh %çá');
+console.assert(decrypt('@# levh %çá', 30) === '@# hard %çá');
+console.assert(encrypt('@# HARD %çá', 30) === '@# levh %çá');
