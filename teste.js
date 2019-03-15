a296eb181de44426094c6a6dd203dca20f8e3574
Julia Pontes <47482821+juliapcp@users.noreply.github.com>
2019-03-15 10:59:42 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,8 +0,0 @@
-for (let x = 0; x < 10; x++) {
-    for (let i = 0; i <= x; i++) {
-        document.write('*');
-        console.log(i + 'dentro');
-    }
-    document.write('<br />');
-    console.log(x + 'fora');
-}
